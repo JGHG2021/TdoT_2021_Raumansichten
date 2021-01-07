2db1d65d598d42262b06f6f5a0188ea051bd9ffa
JGHG2021 <76944756+JGHG2021@users.noreply.github.com>
2021-01-07 11:02:21 +0100

    files

---
+++ app-files/data.js
@@ -0,0 +1,734 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-foyer-haus-b",
+      "name": "Foyer Haus B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.1895820422803247,
+        "pitch": 0.07568530615385427,
+        "fov": 1.409312597347647
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.836841095115818,
+          "pitch": -0.14445667550363694,
+          "title": "Gestaltung Haus B<br>",
+          "text": "Das Kunstprojekt \"10 000 Fragen\" wurde von Adib Fricke 2016 realisiert. Die Fragen, die im ganzen Haus B an den Wänden sichtbar sind, wurden von Lernenden unserer Schule gestellt, vom Künstler gesammelt und anschließend verewigt.<br>"
+        },
+        {
+          "yaw": 2.2140736767959925,
+          "pitch": -0.1618049013384386,
+          "title": "Solaranlage",
+          "text": "Das Lichtenberg-Gymnasium besaß unter anderem eine Solaranlage, die nach der Schulfusion mit dem Herder-Gymnasium übernommen wurde.<br>"
+        },
+        {
+          "yaw": 2.4142849778411755,
+          "pitch": -0.15825317187284682,
+          "title": "Vertretungsplan",
+          "text": "Der Vertretungsplan ist im Schulhaus und per App einsehbar.<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-sozialpdagogischer-bereich",
+      "name": "Sozialpädagogischer Bereich",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.38244496636031755,
+          "pitch": -0.16017515275613903,
+          "title": "Sozialpädagogischer Bereich<br>",
+          "text": "<div>Weiterführende Informationen finden Sie hier:</div><div>https://www.jgherder.de/sozialpaedagogischer-bereich/</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-englisch",
+      "name": "Englisch",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.06695698545095041,
+        "pitch": 0.05611152007957898,
+        "fov": 1.409312597347647
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 3.100885925269772,
+          "pitch": -0.3731948152266895,
+          "title": "Sanierung",
+          "text": "Bei der Sanierung des Schulhauses wurden die Unterrichtsräume u.a. mit einer automatisierten Lichtanlage ausgestattet.<br>"
+        },
+        {
+          "yaw": 0.4347890599387352,
+          "pitch": -0.00501263762488513,
+          "title": "GAPP",
+          "text": "Im\n Rahmen des German American Partnership Program (GAPP) bieten wir Austauschprogramme für die Sek I mit der Kealing\n Middle School in Austin, Texas, sowie für die Sek II mit der Liberal \nArts and Science Academy, ebenfalls in Austin, Texas, an."
+        },
+        {
+          "yaw": -0.6746596541784147,
+          "pitch": 0.21852189321403515,
+          "title": "CertiLingua",
+          "text": "Bei uns kann das Exzellenzlabel CertiLingua erworben werden. Nur insgesamt 8 Schulen in Berlin bieten dies an.<br>"
+        },
+        {
+          "yaw": 0.5410785524766872,
+          "pitch": 0.3528004015279791,
+          "title": "Zertifikatserwerb",
+          "text": "<div>An unserer Schule können Zertifikate in Englisch, Russisch und Französisch erworben werden.</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-franzsisch",
+      "name": "Französisch",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.2263826963921396,
+          "pitch": -0.26997536660018184,
+          "title": "SMART Boards<br>",
+          "text": "Mittlerweile sind 33 Unterrichtsräume mit digitalen Whiteboards ausgestattet. Die übrigen Räume sind alle mit einem PC und Beamer ausgestattet. Jedes Schuljahr werden neue Geräte angeschafft.<br>"
+        },
+        {
+          "yaw": 0.6917231602076797,
+          "pitch": 0.05451600497063325,
+          "title": "Austausche/Fahrten<br>",
+          "text": "Im Rahmen unseres auf Sprachen ausgerichteten Profils bieten wir auch Fahrten bzw. Austausche nach Frankreich und Spanien an, u.a. nach Paris und Sevilla.<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-geschichte",
+      "name": "Geschichte",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.23885937860701034,
+          "pitch": -0.21632400771170524,
+          "title": "Bilingualer Unterricht<br>",
+          "text": "Aktuell bietet unsere Schule in Geografie, Geschichte und Politik bilingualen Unterricht in der Zielsprache Englisch an.<br>"
+        }
+      ]
+    },
+    {
+      "id": "5-naturwissenschaften",
+      "name": "Naturwissenschaften",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.03189780733507419,
+        "pitch": 0.010439352572944927,
+        "fov": 1.409312597347647
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.23929730790434078,
+          "pitch": -0.2430588692927138,
+          "title": "Projekte",
+          "text": "Über den \"normalen\" Unterricht hinaus bietet der Nawi-Bereich vielfältige Projekte an, z.B. das \"Hebelprojekt\" oder das Projekt \"Chill mal im Alltag\".<br>"
+        },
+        {
+          "yaw": -0.5651130592666131,
+          "pitch": 0.21599833522189726,
+          "title": "Leistungskurs Physik<br>",
+          "text": "Obwohl unsere Schule ein Sprachenprofil hat, bieten wir einen Leistungskurs Physik an.<br>"
+        }
+      ]
+    },
+    {
+      "id": "6-chemie",
+      "name": "Chemie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.002341708212592,
+        "pitch": -0.01128482154498478,
+        "fov": 1.409312597347647
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.197778503876675,
+          "pitch": 0.25480426091214525,
+          "title": "Laptops",
+          "text": "Unsere Schule verfügt über mehrere Laptopwägen sowie über W-Lan im gesamten Schulhaus.<br>"
+        }
+      ]
+    },
+    {
+      "id": "7-biologie",
+      "name": "Biologie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-musik",
+      "name": "Musik",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.020954066429609952,
+        "pitch": 0.03261906792295122,
+        "fov": 1.409312597347647
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.06176622453079439,
+          "pitch": -0.21665636558370416,
+          "title": "musikalische Highlights<br>",
+          "text": "Der Fachbereich Musik organisiert in jedem Schuljahr muikalische \nHighlights, wie z.B. das Weihnachtskonzert, das traditionell in der \nErlöserkirche Rummelsburg stattfindet."
+        },
+        {
+          "yaw": -1.002455862172308,
+          "pitch": 0.0726113383737772,
+          "title": "Schulhof<br>",
+          "text": "Auf dieser Seite des Schulhofs ist der sog. \"grüne Hof\". Auf der anderen Seite ist der \"Sporthof\", auf dem u.a. eine Tartanbahn sowie ein Basketball- und Fußballfeld zu finden sind.<br>"
+        }
+      ]
+    },
+    {
+      "id": "9-darstellendes-spiel",
+      "name": "Darstellendes Spiel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.34099997991387276,
+          "pitch": -0.17417037401648017,
+          "title": "TUSCH",
+          "text": "<div>Unsere Schule ist Partnerschule beim TUSCH-Programm. Näheres unter:</div><div>https://www.tusch-berlin.de<br></div>"
+        }
+      ]
+    },
+    {
+      "id": "10-kunst",
+      "name": "Kunst",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.7326697319354665,
+          "pitch": -0.19107273790175938,
+          "title": "Kulturagenten",
+          "text": "<div>Unsere Schule nimmt seit 2016 am Kulturagenten-Programm teil. Informationen dazu finden Sie unter:</div><div>https://padlet.com/kulturagentenberlin/Wissenswerkzeug_Kulturagenten<br></div>"
+        },
+        {
+          "yaw": -0.49286908890852743,
+          "pitch": -0.0604721482269035,
+          "title": "Projekte und Austellungen<br>",
+          "text": "Der Fachbereich Kunst präsentiert regelmäßig Arbeiten aus dem Unterricht in Austellungen oder auf der Homepage. Außerdem gibt es jedes Jahr einen Kunstkalender zu erwerben.<br>"
+        }
+      ]
+    },
+    {
+      "id": "11-pc-raum",
+      "name": "PC-Raum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.4816937609605425,
+          "pitch": 0.023717591113141978,
+          "title": "PC-Räume",
+          "text": "Neben den Laptops verfügt unsere Schule über drei PC-Räume. Die Computer werden von einer Lehrkraft und einem Techniker gewartet. Die Technik wird ständig erweitert und verbessert.<br>"
+        }
+      ]
+    },
+    {
+      "id": "12-mensa",
+      "name": "Mensa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.05933606402213343,
+          "pitch": -0.032539917079358105,
+          "title": "Essensangebot",
+          "text": "Unser Mittagessen wird vom Anbieter DLS frisch zubereitet und über ein Buffet-System ausgegeben.<br>"
+        }
+      ]
+    },
+    {
+      "id": "13-schulbibliothek",
+      "name": "Schulbibliothek",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.3941853849493988,
+        "pitch": 0.2983213374373399,
+        "fov": 1.409312597347647
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.26798723586311,
+          "pitch": 0.5237166206153283,
+          "title": "Schulbibliothek",
+          "text": "Die Schulbibliothek wird von Lernenden selbst gestaltet und verwaltet.<br>"
+        }
+      ]
+    },
+    {
+      "id": "14-oberstufenraum",
+      "name": "Oberstufenraum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.9092636935276914,
+        "pitch": 0.2234438660219631,
+        "fov": 1.409312597347647
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.238445108022951,
+          "pitch": -0.08269320049751094,
+          "title": "Oberstufenraum",
+          "text": "Aktuell gibt es zwei Oberstufenräume, die während der Hofpausen und in Freiblöcken von Schüler*innen der Sek II genutzt werden können.<br>"
+        }
+      ]
+    },
+    {
+      "id": "15-innenansicht-neue-turnhalle",
+      "name": "Innenansicht neue Turnhalle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.7360500638004908,
+        "pitch": -0.005478984616523519,
+        "fov": 1.409312597347647
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.1611340547514768,
+          "pitch": -0.027654193566899465,
+          "title": "neue Turnhalle<br>",
+          "text": "Die Turnhalle ist neu saniert, aber noch nicht vollständig an die Schule übergeben worden.<br>"
+        },
+        {
+          "yaw": -1.6462953891339431,
+          "pitch": -0.05139462822165264,
+          "title": "Sportunterricht",
+          "text": "Seit dem Schuljahr 2017/18 unterrichten wir in allen Klassenstufen koedukativ. Besondere Angebote des Fachbereichs sind u.a. die Schneesportfahrt in Klasse 11 und der Top-Rope-Kletterkurs.<br>"
+        }
+      ]
+    },
+    {
+      "id": "16-umkleiden-neue-turnhalle",
+      "name": "Umkleiden neue Turnhalle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-sanitrbereich-neue-turnhalle",
+      "name": "Sanitärbereich neue Turnhalle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "TdoT 2021 Raumansichten",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
